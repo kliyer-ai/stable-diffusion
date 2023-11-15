0dc9e8050ab9c485dd0b7bda9b34568790081ded
kiyer-ai <mail@nickstracke.dev>
2023-11-15 17:04:34 +0100

    use attention rescaling for small finetuning

--- ldm/modules/attention.py
+++ ldm/modules/attention.py
@@ -152,6 +152,7 @@ class SpatialSelfAttention(nn.Module):
 class CrossAttention(nn.Module):
     def __init__(self, query_dim, context_dim=None, heads=8, dim_head=64, dropout=0.):
         super().__init__()
+        self.dim_head = dim_head
         inner_dim = dim_head * heads
         context_dim = default(context_dim, query_dim)
 
@@ -168,6 +169,11 @@ class CrossAttention(nn.Module):
         )
 
     def forward(self, x, context=None, mask=None):
+
+        is_self_attention = context is None
+
+        n_tokens = x.shape[1]
+
         h = self.heads
 
         q = self.to_q(x)
@@ -175,21 +181,12 @@ class CrossAttention(nn.Module):
         k = self.to_k(context)
         v = self.to_v(context)
 
-        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> (b h) n d', h=h), (q, k, v))
+        q, k, v = map(lambda t: rearrange(t, "b n (h d) -> (b h) n d", h=h), (q, k, v))
 
-        sim = einsum('b i d, b j d -> b i j', q, k) * self.scale
+        # initialize with rescaled attention
+        out = F.scaled_dot_product_attention(q, k, v, scale=(math.log(n_tokens) / math.log(n_tokens*4) / self.dim_head)**0.5 if is_self_attention else None)
 
-        if exists(mask):
-            mask = rearrange(mask, 'b ... -> b (...)')
-            max_neg_value = -torch.finfo(sim.dtype).max
-            mask = repeat(mask, 'b j -> (b h) () j', h=h)
-            sim.masked_fill_(~mask, max_neg_value)
-
-        # attention, what we cannot get enough of
-        attn = sim.softmax(dim=-1)
-
-        out = einsum('b i j, b j d -> b i d', attn, v)
-        out = rearrange(out, '(b h) n d -> b n (h d)', h=h)
+        out = rearrange(out, "(b h) n d -> b n (h d)", h=h)
         return self.to_out(out)
 
 
